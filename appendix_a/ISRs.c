6d6fe6402f88c74a73c8b9c5d298dc4bf55f4545
Joshua Edgcombe <joshedgcombe@gmail.com>
2017-11-12 12:41:30 -0500

    Fix formatting

--- appendix_a/ISRs.c
+++ appendix_a/ISRs.c
@@ -56,8 +56,8 @@ static float Output_Magnitude_Total[BUFFER_COUNT] = { 0 };
 #define RIGHT 1
 
 volatile union {
-	Uint32 UINT;
-	Int16 Channel[2];
+  Uint32 UINT;
+  Int16 Channel[2];
 } CodecDataIn, CodecDataOut;
 
 /* add any global variables here */
@@ -99,94 +99,94 @@ void EDMA_Init()
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {
-	EDMA_params* param;
+  EDMA_params* param;
 
-	// McASP tx event params
-	param = (EDMA_params*)EDMA3_0_PARAM(EDMA3_EVENT_MCASP0_TX);
-	param->option = EDMA_CONFIG_TX_OPTION;
-	param->source = (Uint32)(&buffer[2][0]);
-	param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
-	param->dest = EDMA_CONFIG_TX_DEST_ADDR;
-	param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(64) & 0xFFFF);
-	param->c_count = 1;
+  // McASP tx event params
+  param = (EDMA_params*)EDMA3_0_PARAM(EDMA3_EVENT_MCASP0_TX);
+  param->option = EDMA_CONFIG_TX_OPTION;
+  param->source = (Uint32)(&buffer[2][0]);
+  param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
+  param->dest = EDMA_CONFIG_TX_DEST_ADDR;
+  param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(64) & 0xFFFF);
+  param->c_count = 1;
 
-	// set up first tx link param
-	param = (EDMA_params*)EDMA3_0_PARAM(64);
-	param->option = EDMA_CONFIG_TX_OPTION;
-	param->source = (Uint32)(&buffer[0][0]);
-	param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
-	param->dest = EDMA_CONFIG_TX_DEST_ADDR;
-	param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(65) & 0xFFFF);
-	param->c_count = 1;
+  // set up first tx link param
+  param = (EDMA_params*)EDMA3_0_PARAM(64);
+  param->option = EDMA_CONFIG_TX_OPTION;
+  param->source = (Uint32)(&buffer[0][0]);
+  param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
+  param->dest = EDMA_CONFIG_TX_DEST_ADDR;
+  param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(65) & 0xFFFF);
+  param->c_count = 1;
 
-	// set up second tx link param
-	param = (EDMA_params*)EDMA3_0_PARAM(65);
-	param->option = EDMA_CONFIG_TX_OPTION;
-	param->source = (Uint32)(&buffer[1][0]);
-	param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
-	param->dest = EDMA_CONFIG_TX_DEST_ADDR;
-	param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(66) & 0xFFFF);
-	param->c_count = 1;
+  // set up second tx link param
+  param = (EDMA_params*)EDMA3_0_PARAM(65);
+  param->option = EDMA_CONFIG_TX_OPTION;
+  param->source = (Uint32)(&buffer[1][0]);
+  param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
+  param->dest = EDMA_CONFIG_TX_DEST_ADDR;
+  param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(66) & 0xFFFF);
+  param->c_count = 1;
 
-	// set up third tx link param
-	param = (EDMA_params*)EDMA3_0_PARAM(66);
-	param->option = EDMA_CONFIG_TX_OPTION;
-	param->source = (Uint32)(&buffer[2][0]);
-	param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
-	param->dest = EDMA_CONFIG_TX_DEST_ADDR;
-	param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(64) & 0xFFFF);
-	param->c_count = 1;
+  // set up third tx link param
+  param = (EDMA_params*)EDMA3_0_PARAM(66);
+  param->option = EDMA_CONFIG_TX_OPTION;
+  param->source = (Uint32)(&buffer[2][0]);
+  param->a_b_count = EDMA_CONFIG_TX_A_B_COUNT;
+  param->dest = EDMA_CONFIG_TX_DEST_ADDR;
+  param->src_dest_b_index = EDMA_CONFIG_TX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(64) & 0xFFFF);
+  param->c_count = 1;
 
 
-	// McASP rx event params
-	param = (EDMA_params*)(EDMA3_0_PARAM(EDMA3_EVENT_MCASP0_RX));
-	param->option = EDMA_CONFIG_RX_OPTION;
-	param->source = EDMA_CONFIG_RX_SRC_ADDR;
-	param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
-	param->dest = (Uint32)(&buffer[1][0]);
-	param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(67) & 0xFFFF);
-	param->c_count = 1;
+  // McASP rx event params
+  param = (EDMA_params*)(EDMA3_0_PARAM(EDMA3_EVENT_MCASP0_RX));
+  param->option = EDMA_CONFIG_RX_OPTION;
+  param->source = EDMA_CONFIG_RX_SRC_ADDR;
+  param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
+  param->dest = (Uint32)(&buffer[1][0]);
+  param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(67) & 0xFFFF);
+  param->c_count = 1;
 
-	// set up first rx link param
-	param = (EDMA_params*)EDMA3_0_PARAM(67);
-	param->option = EDMA_CONFIG_RX_OPTION;
-	param->source = EDMA_CONFIG_RX_SRC_ADDR;
-	param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
-	param->dest = (Uint32)(&buffer[2][0]);
-	param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(68) & 0xFFFF);
-	param->c_count = 1;
+  // set up first rx link param
+  param = (EDMA_params*)EDMA3_0_PARAM(67);
+  param->option = EDMA_CONFIG_RX_OPTION;
+  param->source = EDMA_CONFIG_RX_SRC_ADDR;
+  param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
+  param->dest = (Uint32)(&buffer[2][0]);
+  param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(68) & 0xFFFF);
+  param->c_count = 1;
 
-	// set up second rx link param
-	param = (EDMA_params*)EDMA3_0_PARAM(68);
-	param->option = EDMA_CONFIG_RX_OPTION;
-	param->source = EDMA_CONFIG_RX_SRC_ADDR;
-	param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
-	param->dest = (Uint32)(&buffer[0][0]);
-	param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(69) & 0xFFFF);
-	param->c_count = 1;
+  // set up second rx link param
+  param = (EDMA_params*)EDMA3_0_PARAM(68);
+  param->option = EDMA_CONFIG_RX_OPTION;
+  param->source = EDMA_CONFIG_RX_SRC_ADDR;
+  param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
+  param->dest = (Uint32)(&buffer[0][0]);
+  param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(69) & 0xFFFF);
+  param->c_count = 1;
 
-	// set up third rx link param
-	param = (EDMA_params*)EDMA3_0_PARAM(69);
-	param->option = EDMA_CONFIG_RX_OPTION;
-	param->source = EDMA_CONFIG_RX_SRC_ADDR;
-	param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
-	param->dest = (Uint32)(&buffer[1][0]);
-	param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
-	param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(67) & 0xFFFF);
-	param->c_count = 1;
+  // set up third rx link param
+  param = (EDMA_params*)EDMA3_0_PARAM(69);
+  param->option = EDMA_CONFIG_RX_OPTION;
+  param->source = EDMA_CONFIG_RX_SRC_ADDR;
+  param->a_b_count = EDMA_CONFIG_RX_A_B_COUNT;
+  param->dest = (Uint32)(&buffer[1][0]);
+  param->src_dest_b_index = EDMA_CONFIG_RX_SRC_DEST_B_INDEX;
+  param->link_reload = (BUFFER_COUNT << 16) + (EDMA3_0_PARAM(67) & 0xFFFF);
+  param->c_count = 1;
 
-	// configure EDMA to start servicing events
-	*(volatile Uint32 *)EDMA3_0_CC_ECR  = EDMA_CONFIG_EVENT_MASK;	// clear pending events
-	*(volatile Uint32 *)EDMA3_0_CC_EESR = EDMA_CONFIG_EVENT_MASK;	// enable events
-	*(volatile Uint32 *)EDMA3_0_CC_DRAE1 = EDMA_CONFIG_EVENT_MASK;	// enable events for region 1
-	*(volatile Uint32 *)EDMA3_0_CC_IESR = EDMA_CONFIG_INTERRUPT_MASK;	// enable CPU interrupt
+  // configure EDMA to start servicing events
+  *(volatile Uint32 *)EDMA3_0_CC_ECR  = EDMA_CONFIG_EVENT_MASK;	// clear pending events
+  *(volatile Uint32 *)EDMA3_0_CC_EESR = EDMA_CONFIG_EVENT_MASK;	// enable events
+  *(volatile Uint32 *)EDMA3_0_CC_DRAE1 = EDMA_CONFIG_EVENT_MASK;	// enable events for region 1
+  *(volatile Uint32 *)EDMA3_0_CC_IESR = EDMA_CONFIG_INTERRUPT_MASK;	// enable CPU interrupt
 }
 
 void ZeroBuffers()
@@ -202,11 +202,11 @@ void ZeroBuffers()
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {
-    Int32 i = BUFFER_COUNT * NUM_BUFFERS;
-    Int32 *p = (Int32 *)buffer;
+  Int32 i = BUFFER_COUNT * NUM_BUFFERS;
+  Int32 *p = (Int32 *)buffer;
 
-    while(i--)
-	*p++ = 0;
+  while(i--)
+    *p++ = 0;
 }
 
 void ProcessBuffer(COMPLEX *twiddle_factors)
@@ -224,72 +224,72 @@ void ProcessBuffer(COMPLEX *twiddle_factors)
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {
-	Int16 *pBuf = buffer[ready_index];
-	Int32 i;
+  Int16 *pBuf = buffer[ready_index];
+  Int32 i;
 
-	// Used for peak finding
-	uint16_t num_peaks = 0;
-	uint16_t peakIndices[BUFFER_COUNT/2] = { 0 };
+  // Used for peak finding
+  uint16_t num_peaks = 0;
+  uint16_t peakIndices[BUFFER_COUNT/2] = { 0 };
 
-	// Extract data from signal
-	for(i = 0;i < BUFFER_COUNT;i++) { // extract data to float buffers
+  // Extract data from signal
+  for(i = 0;i < BUFFER_COUNT;i++) { // extract data to float buffers
 
-	  // Sum left and right signal
-	  Input_Total[i].re = *pBuf + *(pBuf + 1);
-	  Input_Total[i].im = 0.0;
+    // Sum left and right signal
+    Input_Total[i].re = *pBuf + *(pBuf + 1);
+    Input_Total[i].im = 0.0;
 
-	  *pBuf += 2;
-	}
+    *pBuf += 2;
+  }
 
-	WriteDigitalOutputs(0); // set digital outputs low - for time measurement
+  WriteDigitalOutputs(0); // set digital outputs low - for time measurement
 
-	/********* END PRE FFT *********/
+  /********* END PRE FFT *********/
 
-	// Compute FFT's
-	fft_c(BUFFER_COUNT, Input_Total, twiddle_factors);  // Input Total
+  // Compute FFT's
+  fft_c(BUFFER_COUNT, Input_Total, twiddle_factors);  // Input Total
 
-	/********* BEGIN POST FFT *********/
+  /********* BEGIN POST FFT *********/
 
-	// Calculate magnitudes of FFT
-	for(i = 0;i < BUFFER_COUNT;i++) {
-	  Output_Magnitude_Total[i] = pow(pow(Input_Total[i].re, 2.0) + pow(Input_Total[i].im, 2.0), 0.5);
-	}
+  // Calculate magnitudes of FFT
+  for(i = 0;i < BUFFER_COUNT;i++) {
+    Output_Magnitude_Total[i] = pow(pow(Input_Total[i].re, 2.0) + pow(Input_Total[i].im, 2.0), 0.5);
+  }
 
-	// Find all peaks (Identified by being greater than both neighboring magnitudes
-	for(i=1; i < BUFFER_COUNT/2; i++) {
-	  if(Output_Magnitude_Total[i] > Output_Magnitude_Total[i-1] && Output_Magnitude_Total[i] > Output_Magnitude_Total[i+1]) {
-	    peakIndices[num_peaks] = i;
-	    num_peaks += 1;
-	  }
-	}
+  // Find all peaks (Identified by being greater than both neighboring magnitudes
+  for(i=1; i < BUFFER_COUNT/2; i++) {
+    if(Output_Magnitude_Total[i] > Output_Magnitude_Total[i-1] && Output_Magnitude_Total[i] > Output_Magnitude_Total[i+1]) {
+      peakIndices[num_peaks] = i;
+      num_peaks += 1;
+    }
+  }
 
-	uint16_t j = 0;
-	uint16_t localMax = 0;
-	uint16_t localMaxIndex = 0;
-	uint16_t localMaxTemp = 0;
+  uint16_t j = 0;
+  uint16_t localMax = 0;
+  uint16_t localMaxIndex = 0;
+  uint16_t localMaxTemp = 0;
 
-	// Bubble sort... cause it's quick and simple... to write...
-	for(j=0; j < num_peaks; j++) {
-	  localMax = 0;
-	  localMaxIndex = 0;
+  // Bubble sort... cause it's quick and simple... to write...
+  for(j=0; j < num_peaks; j++) {
+    localMax = 0;
+    localMaxIndex = 0;
 
-	  // Find max
-	  for(i=j; i < num_peaks; i++) {
-	    if(Output_Magnitude_Total[peakIndices[i]] > localMax) {
-	      localMax = Output_Magnitude_Total[peakIndices[i]];
-	      localMaxIndex = i;
-	    }
-	  }
+    // Find max
+    for(i=j; i < num_peaks; i++) {
+      if(Output_Magnitude_Total[peakIndices[i]] > localMax) {
+	localMax = Output_Magnitude_Total[peakIndices[i]];
+	localMaxIndex = i;
+      }
+    }
 
-	  // Swap with jth item
-	  localMaxTemp = peakIndices[j];
-	  peakIndices[j] = peakIndices[localMaxIndex];
-	  peakIndices[localMaxIndex] = localMaxTemp;
-	}
+    // Swap with jth item
+    localMaxTemp = peakIndices[j];
+    peakIndices[j] = peakIndices[localMaxIndex];
+    peakIndices[localMaxIndex] = localMaxTemp;
+  }
 
-	/* Your code should be done by here */
-	WriteDigitalOutputs(1); // set digital output bit 0 high - for time measurement
-	buffer_ready = 0; // signal we are done
+  /* Your code should be done by here */
+  WriteDigitalOutputs(1); // set digital output bit 0 high - for time measurement
+  buffer_ready = 0; // signal we are done
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -305,7 +305,7 @@ void ProcessBuffer(COMPLEX *twiddle_factors)
 ///////////////////////////////////////////////////////////////////////
 int IsBufferReady()
 {
-	return buffer_ready;
+  return buffer_ready;
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -321,7 +321,7 @@ int IsBufferReady()
 ///////////////////////////////////////////////////////////////////////
 int IsOverRun()
 {
-	return over_run;
+  return over_run;
 }
 
 interrupt void EDMA_ISR()
@@ -338,12 +338,12 @@ interrupt void EDMA_ISR()
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {
-	*(volatile Uint32 *)EDMA3_0_CC_ICR = EDMA_CONFIG_INTERRUPT_MASK; // clear interrupt
-	if(++ready_index >= NUM_BUFFERS) // update buffer index
-		ready_index = 0;
-	if(buffer_ready == 1) // set a flag if buffer isn't processed in time
-		over_run = 1;
-	buffer_ready = 1; // mark buffer as ready for processing
+  *(volatile Uint32 *)EDMA3_0_CC_ICR = EDMA_CONFIG_INTERRUPT_MASK; // clear interrupt
+  if(++ready_index >= NUM_BUFFERS) // update buffer index
+    ready_index = 0;
+  if(buffer_ready == 1) // set a flag if buffer isn't processed in time
+    over_run = 1;
+  buffer_ready = 1; // mark buffer as ready for processing
 }
 
 interrupt void CODEC_ISR()
@@ -376,40 +376,40 @@ interrupt void CODEC_ISR()
 
   /* add your code starting here */
   switch(waveTypeLeft) {
-    case SINE_WAVE:
-      yLeft = outputGain * sine_wave(running_waveform_index);
-      break;
+  case SINE_WAVE:
+    yLeft = outputGain * sine_wave(running_waveform_index);
+    break;
 
-    case COS_WAVE:
-      yLeft = outputGain * cosine_wave(running_waveform_index);
-      break;
+  case COS_WAVE:
+    yLeft = outputGain * cosine_wave(running_waveform_index);
+    break;
 
-    case SQUARE_WAVE:
-      yLeft = outputGain * square_wave(running_waveform_index);
-      break;
+  case SQUARE_WAVE:
+    yLeft = outputGain * square_wave(running_waveform_index);
+    break;
 
-    case SAWTOOTH_WAVE:
-      yLeft = outputGain * sawtooth_wave(running_waveform_index);
-      break;
-    }
+  case SAWTOOTH_WAVE:
+    yLeft = outputGain * sawtooth_wave(running_waveform_index);
+    break;
+  }
 
   switch(waveTypeRight) {
-    case SINE_WAVE:
-      yRight = outputGain * sine_wave(running_waveform_index);
-      break;
+  case SINE_WAVE:
+    yRight = outputGain * sine_wave(running_waveform_index);
+    break;
 
-    case COS_WAVE:
-      yRight = outputGain * cosine_wave(running_waveform_index);
-      break;
+  case COS_WAVE:
+    yRight = outputGain * cosine_wave(running_waveform_index);
+    break;
 
-    case SQUARE_WAVE:
-      yRight = outputGain * square_wave(running_waveform_index);
-      break;
+  case SQUARE_WAVE:
+    yRight = outputGain * square_wave(running_waveform_index);
+    break;
 
-    case SAWTOOTH_WAVE:
-      yRight = outputGain * sawtooth_wave(running_waveform_index);
-      break;
-    }
+  case SAWTOOTH_WAVE:
+    yRight = outputGain * sawtooth_wave(running_waveform_index);
+    break;
+  }
 
   // Update state for next iteration
   // Update current waveform index position
@@ -419,7 +419,6 @@ interrupt void CODEC_ISR()
   if(running_waveform_index >= MAX_WAVEFORM_INDEX) {
     running_waveform_index -= MAX_WAVEFORM_INDEX;
   }
-
 
   CodecDataOut.Channel[ LEFT] = yLeft;
   CodecDataOut.Channel[RIGHT] = yRight;
