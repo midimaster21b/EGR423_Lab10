0dedd2bcc44251ec0b71804096b7a9ee3c03d824
Joshua Edgcombe <joshedgcombe@gmail.com>
2017-11-14 22:28:22 -0500

    Force arrays to work, fix indexing problem

--- appendix_a/dtfm.c
+++ appendix_a/dtfm.c
@@ -1,19 +1,42 @@
 #include <stdint.h>
 #include "dtfm.h"
 
-const char match_values[DTFM_NUM_ROWS][DTFM_NUM_COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-
 char determine_character(float dtfm_freq_one, float dtfm_freq_two) {
 
+  char match_values[DTFM_NUM_ROWS][DTFM_NUM_COLS];
+
+  match_values[0][0] = '1';
+  match_values[0][1] = '2';
+  match_values[0][2] = '3';
+  match_values[0][3] = 'A';
+  match_values[1][0] = '4';
+  match_values[1][1] = '5';
+  match_values[1][2] = '6';
+  match_values[1][3] = 'B';
+  match_values[2][0] = '7';
+  match_values[2][1] = '8';
+  match_values[2][2] = '9';
+  match_values[2][3] = 'C';
+  match_values[3][0] = '*';
+  match_values[3][1] = '0';
+  match_values[3][2] = '#';
+  match_values[3][3] = 'D';
+
   uint16_t boundries[DTFM_NUM_TONES]  = { 730, 810, 900, 1050, 1270, 1400, 1550, 1800 };
-  uint16_t dtfm_freqs[DTFM_NUM_TONES] = { 697, 770, 852, 941,  1209, 1336, 1477, 1633 }; 
-  uint16_t dtfm_freq_one_num, dtfm_freq_two_num, dtfm_low_freq, dtfm_high_freq, dtfm_margin;
+  /* float dtfm_freqs[DTFM_NUM_TONES] = { 697, 770, 852, 941,  1209, 1336, 1477, 1633 }; */
+  float dtfm_freqs[DTFM_NUM_TONES];
+  uint16_t dtfm_freq_one_num, dtfm_freq_two_num, dtfm_low_freq, dtfm_high_freq;
+  float dtfm_margin;
   uint8_t i;
+
+  dtfm_freqs[0] = 697.0;
+  dtfm_freqs[1] = 770.0;
+  dtfm_freqs[2] = 852.0;
+  dtfm_freqs[3] = 941.0;
+  dtfm_freqs[4] = 1209.0;
+  dtfm_freqs[5] = 1336.0;
+  dtfm_freqs[6] = 1477.0;
+  dtfm_freqs[7] = 1633.0;
 
   // Determine what expected frequencies were
   for(i=0; i<DTFM_NUM_TONES; i++) {
@@ -57,5 +80,5 @@ char determine_character(float dtfm_freq_one, float dtfm_freq_two) {
     return '\0';
   }
 
-  return match_values[dtfm_low_freq][dtfm_high_freq];
+  return match_values[dtfm_low_freq][dtfm_high_freq - 4];
 }
--- appendix_a/dtfm.h
+++ appendix_a/dtfm.h
@@ -3,6 +3,4 @@
 #define DTFM_NUM_ROWS  4
 #define DTFM_NUM_COLS  4
 
-
-
 char determine_character(float dtfm_freq_one, float dtfm_freq_two);
